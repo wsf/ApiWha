4b1829d914f1c62eb8a381cfde5403fe4c696851
Lauro123 <laurocaccia@gmail.com>
2018-11-16 20:02:29 -0300

    readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ApiWha
+Al estar trabajando en local se bloquea el acceso, hay que instalar en chrome: https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi
+
+desde apiwha https://panel.apiwha.com despues de loguear vas a My numbers - Add numbers - get trial.
+despues le das a la tuerca de config del numero que te dió y copias el apikey y lo pegas en enviar.js, apiKey
+en enviar.js pones tu numero usando el formato internacional
+Para poder enviar un mensaje primero tenes que mandarle un mensaje vos, agregalo como contacto anteponiendo el simbolo +
+
+En la consola muestra el resultado del envió y los mensajes recibidos.
--- src/enviar.js
+++ src/enviar.js
@@ -1,9 +1,10 @@
-var numero = "549" //numero en formato internacional 54 9 caracteristica numero
-var apiKey = "28QL1HO22Q4BWQ22PYBW"
+var numero = "549"; //numero en formato internacional 54 9 caracteristica numero
+var apiKey = "28QL1HO22Q4BWQ22PYBW";
+var mensaje = "texto de prueba";
 var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://panel.apiwha.com/send_message.php?apikey="+apiKey+"&number="+numero+"&text=texto de prueba",
+    "url": "https://panel.apiwha.com/send_message.php?apikey="+apiKey+"&number="+numero+"&"+mensaje,
     "method": "GET",
     "headers": {}
   }
@@ -17,12 +18,12 @@ var settings = {
 var receive ={
     "url":"https://panel.apiwha.com/get_messages.php?apikey="+apiKey+"&number="+numero,
 }
-var respuesta;
+
 function recibir()
     {
         return $.ajax(receive).done(function (response) {
             console.log(response);
-            respuesta = response[response.length-1]; //obtiene el ultimo mensaje
-            console.log(respuesta.text); // parametros disponibles creation_date,custom_data,from,id,number,process_date,text,to,type(in/out)
+            var respuesta = response[response.length-1]; //obtiene el ultimo mensaje
+            console.log(respuesta.text); // valores disponibles: creation_date,custom_data,from,id,number,process_date,text,to,type(in/out)
           });
       }
